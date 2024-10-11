a980543f8f8a497641859983e915a04e9b5e9d00
ParthDhabalia1024 <parthd505@gmail.com>
2024-10-11 23:50:51 +0530

    good

---
+++ Basic/ques_18.py
@@ -0,0 +1,17 @@
+#calculate factors
+
+
+
+
+n = int(input())
+# n2 = int(input())
+# n3 = int(input())
+def Factor(n):
+    k=[]
+    for i in range(1, n+1):
+        if n % i == 0:
+            k.append(i)
+    return k
+    
+result = Factor(n)
+print(result)        
